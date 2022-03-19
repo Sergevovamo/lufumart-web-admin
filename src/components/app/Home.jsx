c6308ecfe9410414f0b9fb8607a626fc67f7624a
allistermugaisi <allistermugaisi@gmail.com>
2022-03-19 14:07:33 +0300

    added home content

--- src/components/app/Home.jsx
+++ src/components/app/Home.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+	Box,
+	Card,
+	CardContent,
+	CardActions,
+	Typography,
+	Button,
+} from '@mui/material';
+import styles from '../../css/Home.module.css';
 
 const Home = () => {
 	return (
-		<div className="home-container">
-			<h1 style={{ paddingTop: '5rem' }}>Home</h1>
+		<div className={styles.home_container}>
+			<div className={styles.home_main_content}>
+				<div className={styles.home_left_content}>
+					<div className={styles.card_detail}>
+						<h4 className={styles.card_title}>Total Customer</h4>
+						<div className="card-inner">
+							<Link
+								style={{
+									textDecoration: 'none',
+									textAlign: 'center',
+								}}
+								to="/"
+							>
+								<Button style={{ fontSize: 40 }}>12</Button>
+							</Link>
+						</div>
+					</div>
+					<div className={styles.card_detail}>
+						<h4 className={styles.card_title}>Total Order</h4>
+						<div className="card-inner">
+							<Link
+								style={{ textDecoration: 'none', textAlign: 'center' }}
+								to="/"
+							>
+								<Button style={{ fontSize: 40 }}>36</Button>
+							</Link>
+						</div>
+					</div>
+					<div className={styles.card_detail}>
+						<h4 className={styles.card_title}>Total Product Category</h4>
+						<div className="card-inner">
+							<Link
+								style={{ textDecoration: 'none', textAlign: 'center' }}
+								to="/"
+							>
+								<Button style={{ fontSize: 40 }}>349</Button>
+							</Link>
+						</div>
+					</div>
+					<div className={styles.card_detail}>
+						<h4 className={styles.card_title}>Total Product Brand</h4>
+						<div className="card-inner">
+							<Link
+								style={{ textDecoration: 'none', textAlign: 'center' }}
+								to="/"
+							>
+								<Button style={{ fontSize: 40 }}>100</Button>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
